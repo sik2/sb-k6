4b9a8083c47e2886be75529ce7648e99f9cf6a0b
sik2 <bickwang123@gmail.com>
2025-02-10 17:12:16 +0900

    init

---
+++ src/main/java/com/ll/sbk6/SbK6Application.java
@@ -0,0 +1,13 @@
+package com.ll.sbk6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbK6Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbK6Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/ll/sbk6/SbK6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.sbk6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbK6ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
